078b8b3bc55d9162c4381a08c607786b3ec58db2
narora1 <niharika1712@gmail.com>
2015-11-24 22:41:08 -0800

    changes

--- fetch_calibration/config/feature.py
+++ fetch_calibration/config/feature.py
@@ -1,11 +1,11 @@
 import rosbag
 
-with rosbag.Bag('output_led.bag', 'w') as outbag:
-    for topic, msg, t in rosbag.Bag('output_gripper.bag').read_messages():
+with rosbag.Bag('output_both.bag', 'w') as outbag:
+    for topic, msg, t in rosbag.Bag('output_color.bag').read_messages():
               # This also replaces tf timestamps under the assumption 
               # that all transforms in the message share the same timestamp
               #print 'convert a message'
-              msg.features.remove("gripper_depth_finder")
-              msg.features.append("led_finder")
+              #msg.features.remove("led_finder")
+              msg.features.append("gripper_depth_finder")
               outbag.write(topic, msg, t)
 
